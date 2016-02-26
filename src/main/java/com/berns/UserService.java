b29e3810a417722602806d37af4d0d53d50a673a
berns <bcokee@gmail.com>
2016-02-26 13:59:44 +0800

    Changed returned data format to XML.

--- src/main/java/com/berns/UserService.java
+++ src/main/java/com/berns/UserService.java
@@ -18,52 +18,53 @@ public class UserService {
 
 	@GET
 	@Path("/users")
-//	@Produces(MediaType.APPLICATION_XML)
+	// @Produces(MediaType.APPLICATION_XML)
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<User> getUsers() {
 		return userDao.getAllUsers();
 	}
-	
+
 	@GET
 	@Path("/greet")
-//	@Produces(MediaType.APPLICATION_XML)
-	//@Produces(MediaType.TEXT_XML)
+	// @Produces(MediaType.APPLICATION_XML)
+	// @Produces(MediaType.TEXT_XML)
 	public Response greet() {
 		return Response.status(200).entity("Hey what's up?").build();
 	}
-	
+
 	@GET
 	@Path("/user-{id}")
-//	@Produces(MediaType.APPLICATION_XML)
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_XML)
+	//@Produces(MediaType.APPLICATION_JSON)
 	public User getUser(@PathParam("id") String id) {
 		return userDao.getUser(id);
 	}
-	
+
 	@POST
 	@Path("/create")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public User createEmployee(User user) {
-	    
-	    return userDao.createUser(user);
+
+		return userDao.createUser(user);
 	}
 
-//	@PUT
-//	@Path("/update")
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public Employee updateEmployee(Employee employee) {
-//	    // create logic goes here
-//	    employee.setName(employee.getName() + "updated");
-//	    return employee;
-//	}
+	// @PUT
+	// @Path("/update")
+	// @Consumes(MediaType.APPLICATION_JSON)
+	// @Produces(MediaType.APPLICATION_JSON)
+	// public Employee updateEmployee(Employee employee) {
+	// // create logic goes here
+	// employee.setName(employee.getName() + "updated");
+	// return employee;
+	// }
 
-//	@DELETE
-//	@Path("/delete/{empId}")
-//	public javax.ws.rs.core.Response deleteEmployee(@PathParam("empId") int empId) throws URISyntaxException {
-//	    return javax.ws.rs.core.Response.status(200).entity("Employee with " + empId + "is deleted successfully").build();
-//	}
-	
-	
+	// @DELETE
+	// @Path("/delete/{empId}")
+	// public javax.ws.rs.core.Response deleteEmployee(@PathParam("empId") int
+	// empId) throws URISyntaxException {
+	// return javax.ws.rs.core.Response.status(200).entity("Employee with " +
+	// empId + "is deleted successfully").build();
+	// }
+
 }
